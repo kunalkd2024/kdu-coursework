fb95ed40419e1756f291207128fe0c015e0cebb7
Kunal Madan <kunalmadan0611@gmail.com>
2024-01-15 12:18:54 +0530

    modify test file again

--- backend/assignments/1/src/main/java/org/example/Main.java
+++ backend/assignments/1/src/main/java/org/example/Main.java
@@ -45,7 +45,7 @@ public class Main {
      *
      * @return A {@code JsonNode} containing the loaded data.
      */
-    private static JsonNode loadFromJson(Path path) {
+    private static JsonNode loadFromJson(String path) {
         JsonNode jsonNode = null;
         try {
             ClassLoader classLoader = Main.class.getClassLoader();
