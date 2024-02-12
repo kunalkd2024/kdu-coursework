7fd51dd11ac3284334596d7a000d2f410f62b715
Kunal Madan <kunalmadan0611@gmail.com>
2024-02-12 17:58:25 +0530

    ran tests

--- frontend/assignments/1/cypress.config.js
+++ frontend/assignments/1/cypress.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
   // Configuration for end-to-end (e2e) tests
   e2e: {
     // Base URL for your application
-    baseUrl: "http://localhost:5500/assignments/1",
+    baseUrl: "https://kunal-fe-assgn-1.azureedge.net/kunal-fe-assign1/index.html",
 
     // Setup Node events for visual regression testing using cypress-visual-regression plugin
     setupNodeEvents(on, config) {
@@ -27,8 +27,8 @@ module.exports = defineConfig({
     PASSWORD: "test",
     USERNAME: "test-user",
     NAME: "test-name",
-    HOME_PAGE_URL: "/home-page/index.html",
-    LOGIN_PAGE_URL: "login/index.html",
+    HOME_PAGE_URL: "index.html",
+    LOGIN_PAGE_URL: "index.html",
     REGISTER_PAGE_URL: "/register/index.html",
     TEST_THRESHOLD: 0.35,
   },
